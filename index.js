900b2a43d800a10c3f414f5ef837d41718ba3bf1
Jawad Ali <jawad.ali@repairdesk.co>
2023-04-22 00:17:07 +0500

    getting users and todos from external api and dockerize

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 import express from "express"
 import dotenv from "dotenv"
+import axios from "axios"
 dotenv.config()
 
 
 const app = express()
 
 
-app.get( "/", ( req, res ) => {
+app.get( "/", ( _req, res ) => {
   res.send( "Hello world" )
 } )
+
+app.get( "/users", async ( _req, res ) => {
+  try {
+    const { data } = await axios.get( "https://jsonplaceholder.typicode.com/users" )
+    res.json( data )
+  } catch ( error ) {
+    res.json( error )
+  }
+} )
+app.get( "/todos", async ( _req, res ) => {
+  try {
+    const { data } = await axios.get( "https://jsonplaceholder.typicode.com/todos" )
+    res.json( data )
+  } catch ( error ) {
+    res.json( error )
+  }
+} )
+
+
 console.log( process.env.PORT )
 app.listen( process.env.PORT, () => {
   console.log( 'server started' )
